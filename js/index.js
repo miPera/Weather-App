52ddae295961cd5bece532897f9d4664d52012fd
miPera <migperalta10@gmail.com>
2017-11-06 18:56:33 -0800

    Page now returns users city and country after finding location

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
+/**
+Weather API code credit: https://codepen.io/freeCodeCamp/pen/bELRjV?editors=1010
+*/
+var api = "https://fcc-weather-api.glitch.me/api/current?"; //api url
+
 $(document).ready(function() {
   alert("Obtaining your location...");
-  getLocation();
+  var location = getLocation();
 });
 
 function getLocation() {
@@ -8,9 +13,25 @@ function getLocation() {
     navigator.geolocation.getCurrentPosition(function (position) {
       var lat = "lat=" + position.coords.latitude;
       var lon = "lon=" + position.coords.longitude;
-      alert("Lat: " + lat + ", Lon: " + lon);
+      getWeather({latitude: lat, longitude: lon});
     });
   } else {
     alert("Geolocation is not supported by this browser.");
+    return {latitude: "0", longitude: "0"};
   }
+}
+
+function getWeather(location) {
+  var urlString = api + location.latitude + "&" + location.longitude;
+  
+  $.ajax({
+    url: urlString,
+    type: 'GET',
+    success: function(result) {
+      alert("City: " + result.name + "\n" + result.sys.country);
+    },
+    error: function(result) {
+      alert("ERROR: Weather not found!");
+    }
+  });
 }
